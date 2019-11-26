7e48d26c4e5d4f1b5ec49450c03e4e45b397d6cc
DOKHELL <48280426+DOKHELL@users.noreply.github.com>
2019-11-26 16:01:58 +0200

    testing

--- src/index.js
+++ src/index.js
@@ -1,32 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//some test dev branche
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {isToggleOn: true};
-
-	// Эта привязка обязательна для работы `this` в колбэке.
-	this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn
-    }));
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'Включено' : 'Выключено'}
-      </button>
-    );
-  }
+function CreateList(props) {
+ const arr = props.arr;
+	const list = arr.map((i) => <li>{i}</li>);
+	return list;
 }
+function ShowList(props) {
+	if (props.state) {
+		return (
+			<CreateList arr={[1,2,3,4,5,6,7]}/>
+		)
+	}
+	return null;
+}
+class RenderList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {showList: false};
+		this.RenderListBtn = this.RenderListBtn.bind(this);
+	}
+	RenderListBtn() {
+		this.setState(state => ({
+			showList: !state.showList
+		}))
+	}
 
-ReactDOM.render(
-  <Toggle />,
-  document.getElementById('root')
-);
+	render() {
+		return (
+			<div>
+				<button onClick={this.RenderListBtn}>{this.state.showList ? 'Hide' : 'Show'}</button>
+				<ul>
+					<ShowList state={this.state.showList}/>
+				</ul>
+			</div>
+		)
+	}
+}
+  
+  ReactDOM.render(
+	<RenderList />,
+	document.getElementById('root')
+  );
